bd4c7b7ea291c3e35c55eda90202dad63e45b414
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-04-11 19:40:06 +0200

    update 03_04_serving_html_templates and a website test

---
+++ 03_04_serving_html_templates/cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"go_go/03_04_serving_html_templates/pkg/handlers"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// main is the main function
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
