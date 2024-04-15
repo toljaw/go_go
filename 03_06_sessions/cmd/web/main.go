ef99faed92c697812cfd304a1a4e05db27ad651e
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-04-15 21:25:02 +0200

    complete 03_06_sessions

--- 03_06_sessions/cmd/web/main.go
+++ 03_06_sessions/cmd/web/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"go_go/03_06_sessions/pkg/config"
 	"go_go/03_06_sessions/pkg/handlers"
 	"go_go/03_06_sessions/pkg/render"
+	"time"
 
 	"log"
 	"net/http"
@@ -12,9 +14,21 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main function
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -7,4 +7,7 @@ require (
 	github.com/go-chi/chi/v5 v5.0.12
 )
 
-require github.com/justinas/nosurf v1.1.1 // indirect
+require (
+	github.com/alexedwards/scs/v2 v2.8.0 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
